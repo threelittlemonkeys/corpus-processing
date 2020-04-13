adb5d24f954ae684eb73bcb07e33c77543b2cae8
threelittlemonkeys <sjlee7642@gmail.com>
2020-04-13 13:00:55 +0900

    Update by commit

--- nlu.py
+++ nlu.py
@@ -2,18 +2,27 @@ import os
 import sys
 from collections import defaultdict
 
-class tokenizer():
+class nlu():
 
     def __init__(self, graph, lexicon):
         self.graph = dict()
         self.lexicon = dict()
-        self.window_size = 0
+        self.char_window_size = 0
         self.debug = True
 
         self.load_graph(graph)
         self.load_lexicon(lexicon)
 
+        self.log("char_window_size = %d" % self.char_window_size)
+        self.log()
+
+    def log(self, *obj):
+        if not self.debug:
+            return
+        print(*obj)
+
     def load_graph(self, filename):
+        self.log("loading graph")
         fo = open(filename)
         for line in fo:
             line = line.strip()
@@ -23,24 +32,25 @@ class tokenizer():
         fo.close()
 
     def load_lexicon(self, filename):
+        self.log("loading lexicon")
         fo = open(filename)
         for line in fo:
             word, tag, *_ = line.strip().split("\t")
             self.lexicon[word] = tag
-            if len(word) > self.window_size:
-                self.window_size = len(word)
+            if len(word) > self.char_window_size:
+                self.char_window_size = len(word)
         fo.close()
 
-    def index(self, sent):
-        idx = [x for x in enumerate(sent) if x[1] != " "]
-        return idx
+    def normalize(self, sent):
+        sent_idx = [x for x in enumerate(sent) if x[1] != " "]
+        return sent_idx
 
-    def tokenize(self, idx):
-        table = [[] for _ in idx]
-        for i in range(len(idx)):
-            k = min(len(idx), i + self.window_size)
-            for j in range(i + 1, k):
-                w = "".join(c for _, c in idx[i:j])
+    def tokenize(self, sent_idx):
+        table = [[] for _ in sent_idx]
+        for i in range(len(sent_idx)):
+            k = min(len(sent_idx), i + self.char_window_size)
+            for j in range(i + 1, k + 1):
+                w = "".join(c for _, c in sent_idx[i:j])
                 if w in self.graph:
                     table[i].append((w, None))
                 if w in self.lexicon:
@@ -48,21 +58,23 @@ class tokenizer():
         return table
 
     def analyze(self, sent):
-        idx = self.index(sent)
-        table = self.tokenize(idx)
-        print("sent =", sent)
-        print("norm =", "".join(c for _, c in idx))
+        sent_idx = self.normalize(sent)
+        table = self.tokenize(sent_idx)
+        self.log("sent =", sent)
+        self.log("norm =", "".join(c for _, c in sent_idx))
         for i, x in enumerate(table):
-            print("table[%d] =" % i, x)
+            self.log("table[%d] =" % i, x)
         input()
 
 if __name__ == "__main__":
-    tokenizer = tokenizer(
-        lexicon = sys.path[0] + "/lexicon.csv",
-        graph = sys.path[0] + "/graph.csv"
+    if len(sys.argv) != 2:
+        sys.exit("Usage: %s text" % sys.argv[0])
+    nlu = nlu(
+        lexicon = "lexicon.csv",
+        graph = "graph.csv"
     )
     fo = open(sys.argv[1])
     for line in fo:
         line = line.strip()
-        tokenizer.analyze(line)
+        nlu.analyze(line)
     fo.close()
