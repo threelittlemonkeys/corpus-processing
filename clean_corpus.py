f706676edca76957ca1e40af87559cb24b9ac5f9
threelittlemonkeys <sjlee7642@gmail.com>
2020-06-10 16:57:05 +0900

    Update by commit

--- clean_corpus.py
+++ clean_corpus.py
@@ -2,9 +2,9 @@ import sys
 import re
 from collections import defaultdict
 
-HW_KATAKANA = "｡ ｢ ｣ ､ ･ ｳﾞ ｶﾞ ｷﾞ ｸﾞ ｹﾞ ｺﾞ ｻﾞ ｼﾞ ｽﾞ ｾﾞ ｿﾞ ﾀﾞ ﾁﾞ ﾂﾞ ﾃﾞ ﾄﾞ ﾊﾞ ﾋﾞ ﾌﾞ ﾍﾞ ﾎﾞ ﾊﾟ ﾋﾟ ﾌﾟ ﾍﾟ ﾎﾟ ｦ ｧ ｨ ｩ ｪ ｫ ｬ ｭ ｮ ｯ ｰ ｱ ｲ ｳ ｴ ｵ ｶ ｷ ｸ ｹ ｺ ｻ ｼ ｽ ｾ ｿ ﾀ ﾁ ﾂ ﾃ ﾄ ﾅ ﾆ ﾇ ﾈ ﾉ ﾊ ﾋ ﾌ ﾍ ﾎ ﾏ ﾐ ﾑ ﾒ ﾓ ﾔ ﾕ ﾖ ﾗ ﾘ ﾙ ﾚ ﾛ ﾜ ﾝ ﾞ ﾟ".split(" ")
-FW_KATAKANA = "。 「 」 、 ・ ヴ ガ ギ グ ゲ ゴ ザ ジ ズ ゼ ゾ ダ ヂ ヅ デ ド バ ビ ブ ベ ボ パ ピ プ ペ ポ ヲ ァ ィ ゥ ェ ォ ャ ュ ョ ッ ー ア イ ウ エ オ カ キ ク ケ コ サ シ ス セ ソ タ チ ツ テ ト ナ >ニ ヌ ネ ノ ハ ヒ フ ヘ ホ マ ミ ム メ モ ヤ ユ ヨ ラ リ ル レ ロ ワ ン ゛ ゜".split(" ")
-HW_TO_FW_KATAKANA = {h: f for h, f in zip(HW_KATAKANA, FW_KATAKANA)}
+HWK = "｡｢｣､･ｳﾞｶﾞｷﾞｸﾞｹﾞｺﾞｻﾞｼﾞｽﾞｾﾞｿﾞﾀﾞﾁﾞﾂﾞﾃﾞﾄﾞﾊﾞﾋﾞﾌﾞﾍﾞﾎﾞﾊﾟﾋﾟﾌﾟﾍﾟﾎﾟｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ" # half width Katakana
+FWK = "。「」、・ヴガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヲァィゥェォャュョッーアイウエオカキクケコサシスセソタチツテトナ>ニヌネノハヒフヘホマミムメモヤユヨラリルレロワン゛゜" # full width Katakana
+H2FK = {h: f for h, f in zip(HWK, FWK)}
 
 def clean_corpus(filename, options):
     fo = open(filename)
@@ -33,8 +33,8 @@ def clean_corpus(filename, options):
         while i < len(line):
             k = True
             for j in (2, 1):
-                if line[i:i + j] in HW_KATAKANA:
-                    _line += HW_TO_FW_KATAKANA[line[i:i + j]]
+                if line[i:i + j] in HWK:
+                    _line += H2FK[line[i:i + j]]
                     k = False
                     i += j
                     break
