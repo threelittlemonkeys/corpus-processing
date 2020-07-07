4972eb4265b2fba432f7d6d188e47fe7d146f4f3
threelittlemonkeys <sjlee7642@gmail.com>
2020-07-07 17:00:59 +0900

    Update by commit

--- tsv2xlsx.py
+++ tsv2xlsx.py
@@ -6,13 +6,10 @@ def tsv2xlsx(filename):
     fi = open(filename)
     fo = xlsxwriter.Workbook(filename + ".xlsx")
     worksheet = fo.add_worksheet()
-    for idx, text in enumerate(fi):
-        _text = list()
-        for x in text.split("\t"):
-            x = x.strip()
-            x = re.sub('^(?=["=])', "'", x)
-            _text.append(x)
-        worksheet.write_row(idx, 0, _text)
+    for i, row in enumerate(fi):
+        for j, col in enumerate(row.split("\t")):
+            col = col.strip()
+            worksheet.write(i, j, col)
     fo.close()
     fi.close()
 
