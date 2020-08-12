7da5463af48f8db19c11441d64816df98146f21c
threelittlemonkeys <sjlee7642@gmail.com>
2020-08-12 16:18:26 +0900

    Update by commit

--- tsv2xlsx.py
+++ tsv2xlsx.py
@@ -2,13 +2,24 @@ import sys
 import re
 import xlsxwriter
 
+def convert(x):
+    try:
+        return int(x)
+    except:
+        pass
+    try:
+        return float(x)
+    except:
+        pass
+    return x
+
 def tsv2xlsx(filename):
     fi = open(filename)
     fo = xlsxwriter.Workbook(filename + ".xlsx")
     worksheet = fo.add_worksheet()
     for i, row in enumerate(fi):
         for j, col in enumerate(row.split("\t")):
-            col = col.strip()
+            col = convert(col.strip())
             worksheet.write(i, j, col)
     fo.close()
     fi.close()
