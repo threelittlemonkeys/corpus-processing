12eec0d3f7839cf7b3f541879461f66cbfed0617
threelittlemonkeys <sjlee7642@gmail.com>
2020-08-24 16:38:58 +0900

    Update by commit

---
+++ compare_bitext.py
@@ -0,0 +1,74 @@
+import sys
+import re
+
+def normalize(x):
+    x = re.sub("[ \f\n\r\v\u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]", "", x)
+    x = x.lower()
+    return x
+
+def filter(action, key, fn_txt, fn_ref):
+
+    pool = {}
+    fo = open(fn_ref)
+    for line in fo:
+        norm = normalize(line)
+        src, tgt = norm.split("\t")
+        pool[src] = True
+        pool[tgt] = True
+    fo.close()
+
+    num_sents = 0
+    num_errors = 0
+
+    fo = open(fn_txt)
+    for line in fo:
+        line = line.strip()
+        norm = normalize(line)
+
+        if norm.count("\t") != 1:
+            num_errors += 1
+            continue
+
+        num_sents += 1
+        src, tgt = norm.split("\t")
+
+        if src == "" or tgt == "":
+            num_errors += 1
+            continue
+
+        if action == "dup":
+            if key == "src" and src in pool:
+                print(line)
+                continue
+            if key == "tgt" and tgt in pool:
+                print(line)
+                continue
+            if key == "any" and src in pool or tgt in pool:
+                print(line)
+                continue
+            if key == "both" and src in pool and tgt in pool:
+                print(line)
+                continue
+
+        if action == "uniq":
+            if key == "src" and src not in pool:
+                print(line)
+                continue
+            if key == "tgt" and tgt not in pool:
+                print(line)
+                continue
+            if key == "any" and src not in pool or tgt not in pool:
+                print(line)
+                continue
+            if key == "both" and src not in pool and tgt not in pool:
+                print(line)
+                continue
+
+    fo.close()
+    sys.stderr.write("%d sentences\n" % num_sents)
+    sys.stderr.write("%d errors\n" % num_errors)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit("Usage: %s dup|uniq src|tgt|any|both text reference" % sys.argv[0])
+    filter(*sys.argv[1:])
