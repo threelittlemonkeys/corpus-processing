bca4337d2922e1018d40986d9aff15c345329ce6
threelittlemonkeys <sjlee7642@gmail.com>
2021-08-30 17:20:44 +0900

    Update by commit

--- delete_text_by_id.py
+++ delete_text_by_id.py
@@ -1,26 +1,22 @@
 import sys
 import re
 
-def replace_text_by_id(filename):
+def delete_text_by_id(filename):
 
     ref = dict()
     fo = open(filename)
     for line in fo:
-        idx, txt = line.split("\t", 1)
-        if idx not in ref:
-            ref[idx] = list()
-        ref[idx].append(txt)
+        idx = line.strip()
+        ref[idx] = True
     fo.close()
 
     for line in sys.stdin:
         idx, _ = line.split("\t", 1)
         if idx in ref:
-            for txt in ref[idx]:
-                print(idx, txt, sep = "\t", end = "")
             continue
         print(line, end = "")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("Usage: %s ref < txt" % sys.argv[0])
-    replace_text_by_id(*sys.argv[1:])
+    delete_text_by_id(*sys.argv[1:])
