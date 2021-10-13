305ad3b8b2edb9efc21121e8b1756280427b38bf
threelittlemonkeys <sjlee7642@gmail.com>
2021-10-13 15:56:59 +0900

    Update by commit

--- tsv2csv.py
+++ tsv2csv.py
@@ -2,7 +2,7 @@ import sys
 import csv
 
 if len(sys.argv) != 2:
-    sys.exit("Usage: %s tsv_file")
+    sys.exit("Usage: %s tsv_file" % sys.argv[0])
 
 with open(sys.argv[1]) as fin:
     with open(sys.argv[1] + ".csv", "w") as fout:
