8fda31dfa733e59f43afaf84374ba2ee72d691e2
threelittlemonkeys <sjlee7642@gmail.com>
2021-11-01 13:16:26 +0900

    Update by commit

--- sort_tsv.py
+++
@@ -1,8 +0,0 @@
-import sys
-import re
-
-for line in sys.stdin:
-    line = line.strip()
-    a, *b = line.split("\t")
-    b = [re.sub("\s+", " ", w.strip()) for w in b]
-    print(a, *sorted(set(b)), sep = "\t")
