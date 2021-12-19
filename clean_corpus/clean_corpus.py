2d1e72f439239bb1df855786664a825e91b627da
threelittlemonkeys <sjlee7642@gmail.com>
2021-12-19 15:12:33 +0900

    Update by commit

--- clean_corpus/clean_corpus.py
+++ clean_corpus/clean_corpus.py
@@ -4,64 +4,72 @@ import re
 import html
 import time
 
-path = (os.path.dirname(__file__) or ".") + "/"
-CONV = dict()
-with open(path + "char_table.tsv") as fo:
-    for line in fo:
-        a, b = line.strip().split("\t")
-        CONV[a] = b
-CONV_MAXLEN = len(max(CONV.keys(), key = len))
+class corpus_cleaner():
 
-def clean_text(line, verbose = False):
+    def __init__(self):
+        self.path = (os.path.dirname(__file__) or ".") + "/"
+        self.table, self.maxlen = self.load_table("char_table.tsv")
 
-    # full width characters
-    line = "".join(chr(ord(c) - 0xFEE0) if "\uFF01" <= c <= "\uFF5E" else c for c in line)
+    def load_table(self, filename):
+        table = dict()
+        with open(self.path + filename) as fo:
+            for line in fo:
+                a, b = line.strip().split("\t")
+                table[a] = b
+        maxlen = len(max(table.keys(), key = len))
+        return table, maxlen
 
-    # mixed width CJK characters
-    i = 0
-    while i < len(line):
-        for j in range(min(len(line), i + CONV_MAXLEN), i, -1):
-            w = line[i:j]
-            if w in CONV:
-                line = line[:i] + CONV[w] + line[j:]
-                i += len(CONV[w])
-                break
-        else:
-            i += 1
+    def clean(self, line):
 
-    # HTML entities
-    line = html.unescape(line)
+        # full width characters
+        line = "".join(chr(ord(c) - 0xFEE0) if "\uFF01" <= c <= "\uFF5E" else c for c in line)
 
-    # control characters
-    line = re.sub("[\x00-\x1F\x7F\x80-\x9F]+", " ", line)
+        # mixed width CJK characters
+        i = 0
+        while i < len(line):
+            for j in range(min(len(line), i + self.maxlen), i, -1):
+                w = line[i:j]
+                if w in self.table:
+                    line = line[:i] + CONV[w] + line[j:]
+                    i += len(self.table[w])
+                    break
+            else:
+                i += 1
 
-    # whitespace characters
-    line = re.sub("[\x20\xA0\u2000-\u200B\u202F\u205F\u3000]+", " ", line)
+        # HTML entities
+        line = html.unescape(line)
 
-    # private use area
-    line = re.sub("[\uE000-\uF8FF]", " ", line)
+        # control characters
+        line = re.sub("[\x00-\x1F\x7F\x80-\x9F]+", " ", line)
 
-    # byte order marks
-    line = re.sub("[\uFEFF\uFFFE]", " ", line)
+        # whitespace characters
+        line = re.sub("[\x20\xA0\u2000-\u200B\u202F\u205F\u3000]+", " ", line)
 
-    # punctuation marks
-    line = re.sub(r"(?<=\S)’(?=(d|ll|m|re|s|t|ve)\b)", "'", line, flags = re.I)
+        # private use area
+        line = re.sub("[\uE000-\uF8FF]", " ", line)
 
-    line = re.sub(" {2,}", " ", line)
-    line = line.strip()
+        # byte order marks
+        line = re.sub("[\uFEFF\uFFFE]", " ", line)
 
-    return line
+        # punctuation marks
+        line = re.sub(r"(?<=\S)’(?=(d|ll|m|re|s|t|ve)\b)", "'", line, flags = re.I)
+
+        line = re.sub(" {2,}", " ", line)
+        line = line.strip()
+
+        return line
 
 if __name__ == "__main__":
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: %s filename [-v]" % sys.argv[0])
 
+    cleaner = corpus_cleaner()
     verbose = (len(sys.argv) == 3 and sys.argv[2] == "-v")
 
     timer = time.time()
     fo = open(sys.argv[1])
     for ln, raw in enumerate(fo, 1):
-        line = clean_text(raw, verbose)
+        line = cleaner.clean(raw)
         if not verbose:
             print(line)
         elif raw != line:
