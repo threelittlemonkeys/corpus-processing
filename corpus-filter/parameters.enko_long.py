5b57e2846b8f7da59615eae1af038b4cb5094da4
threelittlemonkeys <sjlee7642@gmail.com>
2022-07-04 10:29:27 +0900

    Update by commit

---
+++ corpus-filter/parameters.enko_long.py
@@ -0,0 +1,16 @@
+SRC_LANG = "en"
+TGT_LANG = "ko"
+
+MAX_SENT_LEN = 100
+MIN_SENT_LEN = 1
+MAX_WORD_LEN = 20
+SENT_LEN_RATIO = 3
+
+# mismatch parameters: minimum mismatches to be filtered out (0 if disabled)
+
+LS_MISMATCH = 1
+SYM_MISMATCH = 1
+BR_MISMATCH = 0
+PUNC_MISMATCH = 0
+QUOT_MISMATCH = 0
+NUM_MISMATCH = 0
