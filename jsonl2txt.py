63580f3b2a17c7dfabb928bacfa93d8753e2b760
threelittlemonkeys <sjlee7642@gmail.com>
2022-08-31 14:33:07 +0900

    Update by commit

--- jsonl2txt.py
+++ jsonl2txt.py
@@ -3,36 +3,51 @@ import re
 import ast
 import json
 
+if len(sys.argv) < 2:
+    sys.exit("Usage: %s filename (name1 ...)")
+
+filename = sys.argv[1]
+names = sys.argv[2:]
+
 def normalize(line):
     line = re.sub("\s+", " ", line)
     line = line.strip()
     return line
 
-def dump(obj, key = ""):
+def dump(obj, data, key = ""):
 
     if type(obj) in (int, float):
-        print(key, obj, sep = "\t")
+        data.append((key, txt))
 
     if type(obj) == str:
         for txt in obj.split("\n"):
             txt = normalize(txt)
             if txt == "":
                 continue
-            print(key, txt, sep = "\t")
+            data.append((key, txt))
 
     if type(obj) == list:
         for e in obj:
-            dump(e, key)
+            dump(e, data, key)
 
     if type(obj) == dict:
         for k, v in obj.items():
-            dump(v, (key + "." if key else "") + k)
+            dump(v, data, (key + "." if key else "") + k)
 
-with open(sys.argv[1]) as fo:
+with open(filename) as fo:
+
     for line in fo:
+
         try:
             line = json.loads(line)
-            dump(line)
         except:
             line = ast.literal_eval(line)
-            dump(line)
+
+        data = []
+        dump(line, data)
+
+        for item in data:
+            k, v = item
+            if names and k not in names:
+                continue
+            print(k, v, sep = "\t")
