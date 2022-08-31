16b442f002f06062aafe1be0a1b389eb79ca3ed7
threelittlemonkeys <sjlee7642@gmail.com>
2022-08-31 11:33:17 +0900

    Update by commit

--- json2txt.py
+++ json2txt.py
@@ -3,9 +3,6 @@ import re
 import ast
 import json
 
-pl = {}
-RE_AL = re.compile("[A-Za-z\uAC00-\uD7AF]")
-
 def normalize(line):
     line = re.sub("\s+", " ", line)
     line = line.strip()
@@ -18,27 +15,21 @@ def dump(obj):
         return
 
     if type(obj) == str:
-        for line in obj.split("\n"):
-            line = normalize(line)
-            if line == "":
+        for txt in obj.split("\n"):
+            txt = normalize(txt)
+            if txt == "":
                 continue
-            if not RE_AL.search(line):
-                continue
-            if line in pl:
-                continue
-            pl[line] = True
-            print(line)
+            print(txt)
         return
 
     if type(obj) == list:
-        for x in obj:
-            dump(x)
+        for e in obj:
+            dump(e)
         return
 
     if type(obj) == dict:
-        for a, b in obj.items():
-            print(a)
-            dump(b)
+        for k, v in obj.items():
+            dump(v)
         return
 
 with open(sys.argv[1]) as fo:
