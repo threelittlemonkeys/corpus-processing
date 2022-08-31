14cff7e968dd4c15522f2d98bebd4de862517df5
threelittlemonkeys <sjlee7642@gmail.com>
2022-08-31 13:42:58 +0900

    Update by commit

--- json2tsv.py
+++ json2tsv.py
@@ -2,8 +2,8 @@ import sys
 import re
 import json
 
-if len(sys.argv) < 3:
-    sys.exit("Usage: %s filename name1 ...")
+if len(sys.argv) not in (2, 3):
+    sys.exit("Usage: %s filename (name1 ...)")
 
 filename = sys.argv[1]
 names = sys.argv[2:]
@@ -11,18 +11,21 @@ names = sys.argv[2:]
 with open(filename) as fo:
     for line in fo:
         try:
-            jo = json.loads(line)
+            data = json.loads(line)
         except:
             continue
-        if type(jo) != dict:
+        if type(data) != dict:
             continue
-        tsv = []
+        if not names:
+            print(list(data.keys())
+            break
+        cols = []
         for x in names:
             y = ""
-            if x in jo:
-                y = jo[x]
+            if x in data:
+                y = data[x]
             if y:
                 y = y.strip()
                 y = re.sub("\s+", " ", y)
-            tsv.append(y)
-        print(*tsv, sep = "\t")
+            cols.append(y)
+        print(*cols, sep = "\t")
