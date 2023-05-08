c1ed73ae4a1e684acfbc69786b232104690e8eab
threelittlemonkeys <sjlee7642@gmail.com>
2023-05-08 20:35:25 +0900

    Update by commit

--- sentence_tokenizer/preproc_books.py
+++ sentence_tokenizer/preproc_books.py
@@ -3,28 +3,24 @@ import sys
 import re
 from sentence_tokenizer import sentence_tokenizer
 
-PRINT_FILTER = "in"
-assert PRINT_FILTER in ("in", "out")
+FILTER = sys.argv[1]
+assert FILTER in ("in", "out")
 
 def validate(line):
 
-    if re.search("^[a-z]", line):
-        return None, False
-
-    if re.search("[^ A-Za-z0-9,.?!:;$%&—()/'\"\n-]", line):
-        return None, False
-
     line = re.sub("(?<=\.) (?=\.)", "", line)
-    line = re.sub(" (?=[,.?!:;])", "", line)
+    line = re.sub("(?<=\() ", "", line)
+    line = re.sub(" (?=[,.?!:;)])", "", line)
     line = re.sub("(\.\"?)[0-9]+( |$)", "\\1\\2", line)
 
     sents = sentence_tokenizer.tokenize(line)
     wps = [sent.count(" ") + 1 for sent in sents] # words per sentence
 
-    '''
-    if len(sents) == 1:
+    if re.search("^[a-z]", line):
         return sents, False
-    '''
+
+    if re.search("[^ A-Za-z0-9,.?!:;$%&£—()/'\"\n-]", line):
+        return sents, False
 
     if sum(wps) / len(wps) < 8:
         return sents, False
@@ -50,34 +46,40 @@ if __name__ == "__main__":
             continue
 
         pid += 1
-        sents, flag = validate(line)
-
-        if PRINT_FILTER == "out":
-            if sents and not flag:
-                print("\n".join(sents))
-                print()
-            continue
+        out, flag = validate(line)
 
         if not flag:
+            if out and FILTER == "out":
+                print("\n".join(out))
+                print()
             continue
 
-        spp = [len(_sents) for _sents in paragraph] # sentences per paragraph
-
         if pid > prev + 1 != 0:
+
+            spp = [len(sents) for sents in paragraph] # sentences per paragraph
+            flag = False
+
             if len(spp) > 1 and sum(spp) / len(spp) >= 2 or spp[0] >= 10:
-                for _sents in paragraph:
-                    for _sent in _sents:
-                        print(_sent)
+                if FILTER == "in":
+                    flag = True
+            elif FILTER == "out":
+                flag = True
+
+            if flag:
+                for sents in paragraph:
+                    for sent in sents:
+                        print(sent)
                     print()
                 print()
+
             paragraph = []
 
-        paragraph.append(sents)
+        paragraph.append(out)
         prev = pid
 
         '''
         print(f"paragraph[{pid}]")
-        for _sent in sents:
-            print(_sent)
+        for sent in sents:
+            print(sent)
         print()
         '''
