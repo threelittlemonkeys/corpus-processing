89729849a4c5703f4ca809572bf9d91f68f0c99a
threelittlemonkeys <sjlee7642@gmail.com>
2023-11-10 18:45:53 +0900

    Update by commit

--- xl_phrase_alignment/xl_phrase_aligner.py
+++ xl_phrase_alignment/xl_phrase_aligner.py
@@ -16,7 +16,6 @@ class xl_phrase_aligner():
         self.threshold = threshold
         self.verbose = verbose
 
-        self.model = self.load_model()
         self.tokenizer = xl_tokenizer()
         self.tokenizer.import_tagger(src_lang)
         self.tokenizer.import_tagger(tgt_lang)
@@ -28,6 +27,8 @@ class xl_phrase_aligner():
         print(f"threshold = {self.threshold}", file = sys.stderr)
         print(f"tokenizer[{src_lang}] =", self.tokenizer.taggers.get(src_lang), file = sys.stderr)
         print(f"tokenizer[{tgt_lang}] =", self.tokenizer.taggers.get(tgt_lang), file = sys.stderr)
+
+        self.model = self.load_model()
 
     def load_model(self):
 
