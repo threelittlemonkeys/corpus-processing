3193dcfedda32828c9c3b0d6d5ba2ee813070d6f
Seung Jin Lee <sjlee7642@gmail.com>
2024-06-26 08:11:34 +0900

    Update by commit

--- xlsx2tsv.py
+++ xlsx2tsv.py
@@ -1,40 +1,55 @@
 import sys
 import re
-import openpyxl
+import math
+import pandas as pd
 
 def xls2tsv(filename, option = None, sheet_idx = 0):
 
-    workbook = openpyxl.load_workbook(filename)
-    sheet_idx = int(sheet_idx)
+    sheets = pd.read_excel(io = filename, sheet_name = None, header = None)
 
     if not option:
-        print("\n".join(workbook.sheetnames))
+
+        print("\n".join(sheets.keys()))
         return
 
     if option == "dump":
-        for sheet_idx, sheet in enumerate(workbook._sheets):
-            fo = open("%s.sheet_%d.%s.tsv" % (filename, sheet_idx, sheet.title), "w")
-            print_sheet(sheet, fo)
+
+        for sheet_idx, (sheet_name, sheet) in enumerate(sheets.items()):
+            fo = open("%s.sheet_%d.%s.tsv" % (filename, sheet_idx, sheet_name), "w")
+            print_sheet(fo, sheet)
             fo.close()
+        return
 
     if option == "sheet":
-        sheet = workbook._sheets[sheet_idx]
-        fo = open("%s.sheet_%d.%s.tsv" % (filename, sheet_idx, sheet.title), "w")
-        print_sheet(sheet, fo)
-        fo.close()
 
-def print_sheet(sheet, fo):
-    for row in sheet.values:
+        sheet_idx = int(sheet_idx)
+        sheet_name = [*sheets.keys()][sheet_idx]
+        sheet = sheets[sheet_name]
+        fo = open("%s.sheet_%d.%s.tsv" % (filename, sheet_idx, sheet_name), "w")
+        print_sheet(fo, sheet)
+        fo.close()
+        return
+
+def print_sheet(fo, sheet):
+
+    sheet = sheet.replace({float("NaN"): None})
+
+    for row_idx, row in sheet.iterrows():
+
         row = [
-            str(col if col != None else "")
-                .strip()
+            re.sub(r"\s+", " ",
+                str("" if col == None else col)
                 .replace("\t", "\\t")
                 .replace("\n", "\\n")
+            ).strip()
             for col in row
         ]
-        fo.write("%s\n" % "\t".join(row))
+
+        print("\t".join(row), file = fo)
 
 if __name__ == "__main__":
+
     if len(sys.argv) not in [2, 3, 4]:
         sys.exit("Usage: %s filename dump|sheet sheet_idx" % sys.argv[0])
+
     xls2tsv(*sys.argv[1:])
