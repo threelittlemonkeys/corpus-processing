7eaee1e7848b290bb8adf218e24d9f7b373c5233
Seung Jin Lee <sjlee7642@gmail.com>
2024-09-11 10:39:25 +0900

    Update by commit

--- phrase_alignment/phrase_aligner.py
+++ phrase_alignment/phrase_aligner.py
@@ -16,8 +16,8 @@ class phrase_aligner():
         self.tgt_lang = tgt_lang
         self.batch_size = batch_size
         self.window_size = window_size
-        self.alignment_score_threshold = thresholds[0]
-        self.phrase_score_threshold = thresholds[1]
+        self.phrase_score_threshold = thresholds[0]
+        self.alignment_score_threshold = thresholds[1]
         self.verbose = verbose
 
         print(f"src_lang = {src_lang}", file = sys.stderr)
@@ -44,7 +44,7 @@ class phrase_aligner():
 
         return model
 
-    def data_iter(self, batch):
+    def preproc(self, batch):
 
         ps = []
         data = []
@@ -54,16 +54,16 @@ class phrase_aligner():
             x, y = line.split("\t")
             xws = re.sub("\\s+", " ", x).strip().split(" ")
             yws = re.sub("\\s+", " ", y).strip().split(" ")
-            xrs, xps = zip(*phrase_iter(xws, self.window_size))
-            yrs, yps = zip(*phrase_iter(yws, self.window_size))
+            xrs, xps = zip(*phrase_ngrams(xws, self.window_size))
+            yrs, yps = zip(*phrase_ngrams(yws, self.window_size))
             ps.extend(xps)
             ps.extend(yps)
-            data.append((x, xws, xrs, xps, y, yws, yrs, yps))
+            data.append(((x, xws, xrs, xps), (y, yws, yrs, yps)))
 
         i = 0
-        hs = self.model.encode(ps, batch_size = self.batch_size)
+        hs = self.model.encode(ps)
 
-        for x, xws, xrs, xps, y, yws, yrs, yps in data:
+        for (x, xws, xrs, xps), (y, yws, yrs, yps) in data:
 
             xhs = hs[i:i + len(xps)]
             i += len(xps)
@@ -76,7 +76,7 @@ class phrase_aligner():
                 print(f"src_tokens\t{xws}")
                 print(f"tgt_tokens\t{yws}\n")
 
-            yield xws, xrs, xps, xhs, yws, yrs, yps, yhs
+            yield xws, yws, (xrs, xps, xhs), (yrs, yps, yhs)
 
     def sentence_similarity(self, batch):
 
@@ -86,206 +86,57 @@ class phrase_aligner():
             x, y = line.split("\t")
             sents.extend([x, y])
 
-        hs = self.model.encode(sents, batch_size = self.batch_size)
+        hs = self.model.encode(sents)
 
         for i in range(0, len(hs), 2):
             yield cosine_similarity(*hs[i: i + 2])
 
-    def score(self, xws, xrs, xps, xhs, yws, yrs, yps, yhs):
+    def align(self, xws, yws, xs, ys):
 
-        xys = []
+        Wp = []
         Wa = np.zeros((len(xws), len(yws)))
 
-        for xr, xp, xh in zip(xrs, xps, xhs):
-            for yr, yp, yh in zip(yrs, yps, yhs):
-                xy = (cosine_similarity(xh, yh), (xr, yr), (xp, yp))
-                if xy[0] < self.phrase_score_threshold:
+        for xr, xp, xh in zip(*xs):
+            for yr, yp, yh in zip(*ys):
+                phrase_score = cosine_similarity(xh, yh)
+                u = (phrase_score, (xr, yr), (xp, yp))
+                if phrase_score < self.phrase_score_threshold:
                     continue
-                xys.append(xy)
-                Wa[xr[0]:xr[1], yr[0]:yr[1]] += xy[0]
+                Wp.append(u)
+                Wa[xr[0]:xr[1], yr[0]:yr[1]] += phrase_score
 
         Wa_xy = normalize(Wa, axis = 1, method = "softmax")
         Wa_yx = normalize(Wa, axis = 0, method = "softmax")
-        Wa = Wa_xy * Wa_yx
+        Wa = Wa_xy * Wa_yx # alignment scores
+
+        alignment_argmax =  Wa.argmax(axis = 1)
+        alignment_scores = Wa[range(Wa.shape[0]), alignment_argmax]
+        alignment_score = (alignment_scores > self.alignment_score_threshold).mean()
 
         if self.verbose:
 
-            print("alignment_map =")
-            print([[round(y, 4) for y in ys] for ys in Wa])
-            print()
+            # print(f"Wa_xy = {Wa_xy.round(4).tolist()}")
+            # print(f"Wa_yx = {Wa_yx.round(4).tolist()}")
+            # print(f"Wa = {Wa.round(4).tolist()}\n")
 
             print("alignment_scores =")
-            for i in range(Wa.shape[0]):
-                for j in range(Wa.shape[1]):
-                    if Wa[i][j] < self.alignment_score_threshold:
-                        continue
-                    print(f"{Wa[i][j]:.4f} {(i, j)} {(xws[i], yws[j])}")
+            for i, j in enumerate(alignment_argmax):
+                a = Wa[i][j]
+                if a < self.alignment_score_threshold:
+                    continue
+                print(f"{a:.4f} {(i, j)} {(xws[i], yws[j])}")
+
+            # heatmap((Wa_xy, xws, yws), (Wa_yx, xws, yws), (Wa, xws, yws))
+            print("\nheatmap =")
+            text_heatmap(Wa, yws, xws, self.alignment_score_threshold)
             print()
 
-        for k in range(len(xys)):
-            phrase_score, (xr, yr), (xp, yp) = xys[k][:3]
-            alignment_score = 0
-            for i in range(xr[0], xr[1]):
-                for j in range(yr[0], yr[1]):
-                    a = Wa[i][j]
-                    if a < alignment_score:
-                        continue
-                    if a < self.alignment_score_threshold:
-                        continue
-                    alignment_score = a
-                    break
-                else:
-                    continue
-                break
-            alignment_score = (alignment_score > 0)
-            xys[k] = ((alignment_score, phrase_score), *xys[k][1:3])
-
-        if self.verbose:
-            print("phrase_scores =")
-            for xy in xys:
-                (alignment_score, phrase_score), (xr, yr), (xp, yp) = xy
-                if alignment_score < self.alignment_score_threshold:
-                    continue
-                if phrase_score < self.phrase_score_threshold:
-                    continue
-                print(f"{phrase_score:.4f} {(xr, xp)} {(yr, yp)}")
-            print()
-
-        return xws, yws, xys, Wa
-
-    def bijection(self, xws, yws, xys): # linear bijective alignment
-
-        phrases = [[] for _ in xws]
-
-        for xy in xys:
-            score, (xr, yr) = xy
-            phrases[xr[0]].append([score, xr, yr])
-
-        phrases = [max(xys) for xys in phrases if xys]
-        lens = [0, 0]
-
-        for cand in phrases:
-
-            score, xr, yr = cand
-
-            if xr[0] < lens[0] or yr[0] < lens[1]: # phrase collision
-                cand.append(False)
-            else:
-                cand.append(True)
-                lens = [xr[1], yr[1]]
-
-        phrases = [cand[:-1] for cand in phrases if cand[-1]]
-
-        return phrases
-
-    @staticmethod
-    def _compare(x1, x2, y1, y2):
-
-        if y2 <= x1 or x2 <= y1:
-            return "DISJOINT"
-        if y1 == x1 < x2 == y2:
-            return "IDENTICAL"
-        if y1 <= x1 < x2 <= y2:
-            return "SUBSET"
-        if x1 <= y1 < y2 <= x2:
-            return "SUPERSET"
-
-        return "OVERLAP"
-
-    def extraction(self, xws, yws, xys): # non-linear alignment
-
-        _xys = []
-
-        for scores, (xr, yr), (xp, yp) in sorted(xys)[::-1]:
-
-            updates = []
-            removes = []
-
-            for cand in _xys:
-
-                _score, _xr, _yr, _state = cand
-
-                if not _state:
-                    continue
-
-                v = {self._compare(*xr, *_xr), self._compare(*yr, *_yr)}
-
-                if v == {"DISJOINT"}:
-                    continue
-
-                if v == {"SUBSET", "SUPERSET"}:
-                    updates.append(cand)
-                    continue
-
-                if not v - {"IDENTICAL", "SUPERSET"}:
-                    removes.append(cand)
-                    continue
-
-                # phrase collision
-                # OVERLAP or ((DISJOINT or IDENTICAL) and SUBSET)
-                break
-
-            else: # if phrase collision not found
-
-                for cand in updates:
-                    _xr, _yr = cand[1:3]
-                    xr = (min(xr[0], _xr[0]), max(xr[1], _xr[1]))
-                    yr = (min(yr[0], _yr[0]), max(yr[1], _yr[1]))
-                    cand[3] = False
-
-                for cand in removes:
-                    cand[3] = False
-
-                _xys.append([scores[1], xr, yr, True])
-                print(f"-> {scores[1]:.4f} {(xr, xp)} {(yr, yp)}")
-
-        _xys = sorted(
-            [cand[:-1] for cand in _xys if cand[-1]],
-            key = lambda xy: xy[1]
-        )
-
-        return _xys
-
-    def align(self, batch, method):
-
-        for data in self.data_iter(batch):
-
-            xws, yws, xys, Wa = self.score(*data)
-            xys = getattr(self, method)(xws, yws, xys)
-            phrase_scores = []
-            sentence_score = 0
-
-            _xws = [*xws]
-            _yws = [*yws]
-            phrase_scores = []
-            sentence_score = 0
-
-            for idx, (score, xr, yr) in enumerate(xys):
-
-                tag = f"({idx}: "
-                phrase_scores.append(score)
-                sentence_score += (xr[1] - xr[0]) + (yr[1] - yr[0])
-
-                for ws, (i, j) in zip((_xws, _yws), (xr, yr)):
-                    ws[i] = tag + ws[i]
-                    ws[j - 1] += " )"
-
-            sentence_score /= len(xws) + len(yws)
-
-            if self.verbose:
-                print(f"src_aligned\t{" ".join(_xws)}")
-                print(f"tgt_aligned\t{" ".join(_yws)}")
-                print("phrase_scores", phrase_scores, sep = "\t")
-                print("sentence_score", sentence_score, sep = "\t")
-                heatmap(Wa, xws, yws)
-                input()
-
-            yield xws, yws, phrase_scores, sentence_score
+        return alignment_score
 
 if __name__ == "__main__":
 
     if len(sys.argv) not in (5, 6):
-        sys.exit("Usage: %s src_lang tgt_lang sentence|bijection|extraction tokenized_bitext [-v]" % sys.argv[0])
+        sys.exit("Usage: %s src_lang tgt_lang phrase|sentence tokenized_bitext [-v]" % sys.argv[0])
 
     src_lang, tgt_lang, method, filename = sys.argv[1:5]
 
@@ -294,16 +145,27 @@ if __name__ == "__main__":
         tgt_lang = tgt_lang,
         batch_size = 1024,
         window_size = 3,
-        thresholds = (0.01, 0.7),
+        thresholds = (0.5, 0.01),
         verbose = (len(sys.argv) == 6 and sys.argv[5] == "-v")
     )
 
-    data_size = 0
+    dataloader = dataloader(
+        filename = filename,
+        batch_size = aligner.batch_size
+    )
+
     timer = time.time()
 
-    for batch in batch_iter(filename, aligner.batch_size):
+    for batch in dataloader.batch():
 
-        data_size += len(batch)
+        if method == "phrase":
+
+            for line, data in zip(batch, aligner.preproc(batch)):
+                alignment_score = aligner.align(*data)
+                print(alignment_score, line, sep = "\t")
+
+                if aligner.verbose:
+                    input()
 
         if method == "sentence":
 
@@ -312,13 +174,5 @@ if __name__ == "__main__":
             for line, sentence_similarity in zip(batch, sentence_scores):
                 print(sentence_similarity, line, sep = "\t")
 
-        if method in ("bijection", "extraction"):
-
-            aligned = aligner.align(batch, method)
-
-            for line, result in zip(batch, aligned):
-                xws, yws, phrase_scores, sentence_score = result
-                print(sentence_score, line, sep = "\t")
-
-    print("%d lines (%.f seconds)" % (data_size, time.time() - timer), file = sys.stderr)
+    print(f"{dataloader.data_size} lines ({time.time() - timer:.4f} seconds)", file = sys.stderr)
     timer = time.time()
--- phrase_alignment/utils.py
+++ phrase_alignment/utils.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from unicodedata import east_asian_width as eaw
 
 # apt-get install fonts-nanum*
 # fc-cache -fv
@@ -30,24 +31,35 @@ def normalize(x, axis, method):
 
     return np.divide(x, z, out = np.zeros_like(x), where = (z != 0))
 
-def batch_iter(filename, batch_size):
+class dataloader():
 
-    batch = []
+    def __init__(self, filename, batch_size):
 
-    with open(filename) as fo:
+        self.filename = filename
+        self.data_size = 0
+        self.batch_size = batch_size
+
+    def batch(self):
+
+        batch = []
+        fo = open(self.filename)
+
         while True:
             line = fo.readline()
             if line:
                 batch.append(line.strip())
-                if len(batch) < batch_size:
+                if len(batch) < self.batch_size:
                     continue
             if batch:
+                self.data_size += len(batch)
                 yield batch
                 batch.clear()
             if not line:
                 break
 
-def phrase_iter(tokens, phrase_maxlen):
+        fo.close()
+
+def phrase_ngrams(tokens, phrase_maxlen):
 
     for i in range(len(tokens)):
         for j in range(i + 1, min(len(tokens), i + phrase_maxlen) + 1):
@@ -65,15 +77,130 @@ def validate_phrase(phrase):
 
     return True
 
-def heatmap(m, xws, yws):
+def bijection(xws, yws, xys): # linear bijective alignment
 
-    sns.heatmap(
-        data = m,
-        cmap = "Reds",
-        cbar = False,
-        xticklabels = yws,
-        yticklabels = xws,
-        annot = True
+    phrases = [[] for _ in xws]
+
+    for xy in xys:
+        score, (xr, yr) = xy
+        phrases[xr[0]].append([score, xr, yr])
+
+    phrases = [max(xys) for xys in phrases if xys]
+    lens = [0, 0]
+
+    for cand in phrases:
+
+        score, xr, yr = cand
+
+        if xr[0] < lens[0] or yr[0] < lens[1]: # phrase collision
+            cand.append(False)
+        else:
+            cand.append(True)
+            lens = [xr[1], yr[1]]
+
+    phrases = [cand[:-1] for cand in phrases if cand[-1]]
+
+    return phrases
+
+def extraction(xws, yws, xys): # non-linear alignment
+
+    _xys = []
+
+    def _compare(x1, x2, y1, y2):
+
+        if y2 <= x1 or x2 <= y1:
+            return "DISJOINT"
+        if y1 == x1 < x2 == y2:
+            return "IDENTICAL"
+        if y1 <= x1 < x2 <= y2:
+            return "SUBSET"
+        if x1 <= y1 < y2 <= x2:
+            return "SUPERSET"
+
+        return "OVERLAP"
+
+    for scores, (xr, yr), (xp, yp) in sorted(xys)[::-1]:
+
+        updates = []
+        removes = []
+
+        for cand in _xys:
+
+            _score, _xr, _yr, _state = cand
+
+            if not _state:
+                continue
+
+            v = {_compare(*xr, *_xr), _compare(*yr, *_yr)}
+
+            if v == {"DISJOINT"}:
+                continue
+
+            if v == {"SUBSET", "SUPERSET"}:
+                updates.append(cand)
+                continue
+
+            if not v - {"IDENTICAL", "SUPERSET"}:
+                removes.append(cand)
+                continue
+
+            # phrase collision
+            # OVERLAP or ((DISJOINT or IDENTICAL) and SUBSET)
+            break
+
+        else: # if phrase collision not found
+
+            for cand in updates:
+                _xr, _yr = cand[1:3]
+                xr = (min(xr[0], _xr[0]), max(xr[1], _xr[1]))
+                yr = (min(yr[0], _yr[0]), max(yr[1], _yr[1]))
+                cand[3] = False
+
+            for cand in removes:
+                cand[3] = False
+
+            _xys.append([scores[1], xr, yr, True])
+
+    _xys = sorted(
+        [cand[:-1] for cand in _xys if cand[-1]],
+        key = lambda xy: xy[1]
     )
 
+    return _xys
+
+def heatmap(*ms):
+
+    _, axs = plt.subplots(ncols = len(ms))
+
+    for ax, (m, xws, yws) in zip(axs, ms):
+        sns.heatmap(
+            data = m,
+            cmap = "Reds",
+            cbar = False,
+            ax = ax,
+            xticklabels = yws,
+            yticklabels = xws,
+            annot = False
+        )
+
     plt.show()
+
+def text_heatmap(m, xws, yws, threshold):
+
+    xi = [str(i)[-1] for i in range(len(xws))]
+    yi = [str(i)[-1] for i in range(len(yws))]
+
+    hl = "+" + "-" * (len(xws) * 2 + 9) + "+" # horizontal line
+    nd = " " * 7 # indent
+
+    xws = [[c + ("" if eaw(c) == "W" else " ") for c in f"{w:<5.5}"] for w in xws]
+    yws = [f"{w:>5.5}" for w in yws]
+
+    print(hl)
+    print("\n".join(" ".join(
+        ["|", w, i, *[" " if y < threshold else "*" for y in ys], "|"]
+        ) for (ys, i, w) in zip(m, yi, yws)
+    ))
+    print(" ".join(["|", nd, *xi, "|"]))
+    print("\n".join(" ".join(["|", nd[:-1], "".join(cs), "|"]) for cs in zip(*xws)))
+    print(hl)
